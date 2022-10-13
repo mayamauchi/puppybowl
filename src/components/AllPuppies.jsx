3efe17056bbacfb0575060819005abfcc0d5b6e3
Brevan <brev.bdawg@gmail.com>
2022-10-13 14:35:37 -0500

    final-inclass-commit

--- src/components/AllPuppies.jsx
+++ src/components/AllPuppies.jsx
@@ -1,5 +1,4 @@
 import  React  from 'react';
-
 const AllPuppies = (props) => {
     const myPuppies = props.puppyData
     console.log(myPuppies)
@@ -9,7 +8,12 @@ const AllPuppies = (props) => {
             myPuppies.map((puppy) => {
                 return (
                     <div key={`player-${puppy.id}`} className="puppy">
+                        
                         <div>{puppy.name}</div>
+                        <div>{puppy.breed}</div>
+                        <div>{puppy.status}</div>
+                        <img id='puppyImg' src={puppy.imageUrl} alt="image" height={400} width={300}></img>
+                        {/* <div>{puppy.imageUrl}</div> */}
                     </div>
                     
             )}
